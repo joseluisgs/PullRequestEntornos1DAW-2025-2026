77e69c8f9a01e623c463359cc2640c5fcf716f10
Florian308 <geoflorian76xx33@gmail.com>
2025-11-05 23:50:45 +0100

    Añado mi codigo a la lista de ejercicios.

---
+++ ejercicios/GrigoreGeorgicaFlorian.cs
@@ -0,0 +1,15 @@
+//Ejercicio 9: Suma de dos números enteros
+Main {
+
+writeLine(“Dime un número entero:”);
+int num1 =(int) readLine();
+
+writeLine(“Dime otro numero entero:”);
+int num2 =(int) readLine();
+
+int suma = num1 + num2;
+
+writeLine(“La suma de los números es:" + suma);
+
+}
+
