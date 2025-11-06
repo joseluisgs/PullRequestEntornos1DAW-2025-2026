fd51df5c25ba801259b6fe60e8b7286198bba491
isabeltopala <isabel.topala@gmail.com>
2025-11-06 11:42:07 +0100

    He realizado un ejercicio y lo he añadido a la carpeta ejercicios

---
+++ ejercicios/TopalaIsabelLorena.cs
@@ -0,0 +1,11 @@
+Main {
+
+    writeLine(“introduce un número entero”);
+    string input = readLine();
+    int numero = (int) input;
+
+    bool validez = !(edad >= 10 && <=20);
+    string valido = (validez == true) ? “Dentro del rango” : “Fuera de rango”;
+
+    writeLine(“El numero es:” +valido);
+}
