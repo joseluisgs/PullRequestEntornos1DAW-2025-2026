2ea9e42d6044a0ea01aa01f6bda913b812e60be6
danielrequena21 <danielrequenacolon@gmail.com>
2025-11-06 16:41:45 +0100

    Añado mi ejercicio

---
+++ ejercicios/RequenaColonDaniel.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nombre = "Daniel Requena Colón";
+        int edad = 21;
+
+        Console.WriteLine("Nombre: " + nombre);
+        Console.WriteLine("Edad: " + edad);
+    }
+}
